6391d2c1a252863c7833c1c7d805b8b62271898e
Julia1284 <julia123456-84@mail.ru>
2021-09-11 11:17:17 +0300

    commit1

---
+++ script.js
@@ -0,0 +1,9 @@
+function clickLeft(){
+    let image = document.getElementById ('image');
+    image.src = "/assets/kartinki-zolotaya-osen-21.jpg"
+}
+
+function clickRight(){
+    let image = document.getElementById ('image');
+    image.src = "/assets/park-derevya-osen-1893.jpg"
+}
